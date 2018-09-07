51940b49c3de10c3d3054bfed6f366c20d40217a
RAMYA45 <43058763+RAMYA45@users.noreply.github.com>
2018-09-07 12:03:12 +0530

    Add files via upload

---
+++ RAJAN.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    int a;
+    float b,c,d,e,f;
+    a=20;
+    b=3.5f;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    System.out.println("the sum is"+c);
+    System.out.println("the difference is"+d);
+    System.out.println("the multiplication is"+e);
+    System.out.println("the division is"+f);
+
+  }
+}
