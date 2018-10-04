08c49061bdb758ae8b8f880a04b1b9b9b942a33d
RAMYA45 <43058763+RAMYA45@users.noreply.github.com>
2018-10-04 12:28:17 +0530

    Add files via upload

---
+++ even or invalid.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args){
+    int number;
+    Scanner scan=new Scanner(System.in);
+    System.out.println("enter the number you want to check");
+    number=scan.nextInt();
+    scan.close();
+    if(number%2==0)
+      System.out.println(number+" is even");
+      else if(number<0)
+      System.out.println(number+" is invalid");
+    else
+      System.out.println(number+" is odd");
+    
+  }
+}
