e7b3bbc875f34257768c3071b71cfc167b4a2718
RAMYA45 <43058763+RAMYA45@users.noreply.github.com>
2018-10-05 12:42:12 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ class Main {
+  public static void main(String[] args) {
+    int i,n;
+    Scanner read = new Scanner(System.in);
+    n=read.nextInt();
+    for(i=0;i<n;i++)
+    System.out.println("hello");
+
+  }
+}
