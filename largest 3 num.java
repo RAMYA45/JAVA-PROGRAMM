62a2d320f9321f30b3f54ee9f0d44004df343011
RAMYA45 <43058763+RAMYA45@users.noreply.github.com>
2018-10-05 11:54:57 +0530

    Add files via upload

---
+++ largest 3 num.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    float n1=2,n2=4,n3=6;
+    if(n1>=n2 && n2>=n3)
+    System.out.println(n1);
+    else if(n2>=n3 && n2>=n1)
+    System.out.println(n2);
+    else
+    System.out.println(n3);
+  }
+}
